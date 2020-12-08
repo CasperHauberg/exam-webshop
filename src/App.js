3dbfa5d265fb2966e607fcce956f5c852108991b
Casper Hauberg <casperhauberg@icloud.com>
2020-12-08 21:35:21 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./Card/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import firebase from "firebase";
+
+class App extends React.Component {
+  getData() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyC68HML9atAc4tEC2NbJTl-oR4khYZrJN0",
+      authDomain: "slothmachine-303e3.firebaseapp.com",
+      databaseURL: "https://slothmachine-303e3.firebaseio.com",
+      projectId: "slothmachine-303e3",
+      storageBucket: "slothmachine-303e3.appspot.com",
+      messagingSenderId: "729958760674",
+      appId: "1:729958760674:web:00fb7352d98579e55d1ebb",
+    };
+
+    firebase.initializeApp(firebaseConfig);
+
+    var database = firebase.database().ref("/Keys"); // Reference to database path - "/Keys" are the path
+
+    database.once("value").then((snapshot) => {
+      const keys = snapshot.val(); // Snapshot.val() is the actual data
+      let storedKey = Object.keys(keys)[Object.keys(keys).length - 1]; // Turns object keys into an array and returns the last key in the array
+      database = firebase.database().ref("/ActiveKey"); // Change the database refence to active key
+      database.set(storedKey); // Sets the storedKey to the new path.
+    });
+  }
+
+  render() {
+    const titles = [
+      "Sloth Codes",
+      "Sloth Machines",
+      "Sloth Figures",
+      "Sloth Keychains",
+    ];
+
+    return (
+      <div className="App bg-light">
+        <div className="container">
+          <div className="row">{titles.forEach}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Card/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import placeholder from "./placeholder.jpg";
+
+function Card(props) {
+  return (
+    <div className="col-md-3">
+      <div className="card rounded shadow">
+        <img src={placeholder} className="card-img-top mb-2" alt="#" />
+        <div className="card-body">
+          <h5 className="card-title mb-2">{props.title}</h5>
+          <div className="card-text mb-3">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam
+            dolorem repudiandae consequuntur aut. Error debitis consectetur
+            voluptas ex iste ipsam!
+          </div>
+          <a href="" className="btn btn-block btn-primary">
+            Add to Cart
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
