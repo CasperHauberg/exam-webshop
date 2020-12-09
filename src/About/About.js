5d3d942a7b6d2769906c21adf74b9b210be43139
Casper Hauberg <casperhauberg@icloud.com>
2020-12-09 16:13:52 +0100

    A lot of changes

---
+++ src/About/About.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAppleAlt,
+  faDice,
+  faLeaf,
+  faCouch,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
+
+const About = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4 text-center">
+            <FontAwesomeIcon
+              icon={faLeaf}
+              size="lg"
+              color="#007BFF"
+              className="mb-2"
+            />
+            <h3 className="text-primary text-center">Sustainable</h3>
+            <p className="text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis reiciendis minima nobis est quas voluptatibus
+              accusantium temporibus exercitationem cumque voluptate.
+            </p>
+          </div>
+          <div className="col-md-4 text-center">
+            <FontAwesomeIcon
+              className="primary"
+              icon={faDice}
+              size="lg"
+              color="#007BFF"
+              className="mb-2"
+            />
+            <h3 className="text-primary text-center">Exciting</h3>
+            <p className="text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis reiciendis minima nobis est quas voluptatibus
+              accusantium temporibus exercitationem cumque voluptate.
+            </p>
+          </div>
+          <div className="col-md-4 text-center">
+            <FontAwesomeIcon
+              icon={faGlobe}
+              size="lg"
+              color="#007BFF"
+              className="mb-2"
+            />
+            <h3 className="text-primary text-center">Worldwide</h3>
+            <p className="text-secondary">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Blanditiis reiciendis minima nobis est quas voluptatibus
+              accusantium temporibus exercitationem cumque voluptate.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+import classes from "./Landing.module.css";
+
+function Landing(props) {
+  return (
+    <section className={classes.bg}>
+      <div className={classes.overlay}>
+        <h1 className="display-2 text-center">Sloths X Makerspace</h1>
+        <div>
+          <button onClick={props.getData} className="btn btn-primary mr-1">
+            Get Sloth Code
+          </button>
+          <button className="btn btn-primary ml-2">About Sloths</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
